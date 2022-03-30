458965e11e0f8359ee542e7bf6af328ac67b63cd
Rachel Katz <rachel.katz@flatironschool.com>
2022-03-30 16:09:25 -0400

    my working solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+/*~~~~~~~~~~~~~~~~~~~~~MY SOLUTION~~~~~~~~~~~~~~~~*/
+customerName = 'bob';
+bestCustomer = undefined;
+const worstCustomer = 'jon';
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    worstCustomer = 'heidi'
+}
