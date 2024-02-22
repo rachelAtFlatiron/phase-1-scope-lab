7b94baca6024a5e0c17285c8eade134d06f8560f
Rachel Katz <rachel.katz@flatironschool.com>
2024-02-22 12:24:41 -0500

    demo done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer 
+const leastFavoriteCustomer = 'mary'
+
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+}
+
+var setBestCustomer = () => {
+    bestCustomer = 'not bob'
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'tim'
+}
