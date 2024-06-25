db168cab7cd7bd487f5142c70044bcf7ecf5f848
Rachel Katz <rachel.katz@flatironschool.com>
2024-06-25 12:04:13 -0400

    demo done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let customerName = 'bob'
+const leastFavoriteCustomer = 'test'
+
+let upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+}
+
+let setBestCustomer = () => {
+    //other stuff can affect it
+    //implication is that bestCustomer exists in the scope of setBestCustomer
+    //because there is no declaring language it is in global scope
+    bestCustomer = 'not bob'
+}
+
+let overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
+
+let changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'not test'
+}
