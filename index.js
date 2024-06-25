69c1b2b82daa3658f8b795751d6cd6057f235278
Rachel Katz <rachel.katz@flatironschool.com>
2024-06-25 11:58:40 -0400

    commit tree 686c11f7c64fa57100a41eab86271ce858d15bad

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+}
+
+var setBestCustomer = () => {
+    //other stuff can affect it
+    //implication is that bestCustomer exists in the scope of setBestCustomer
+    bestCustomer = 'not bob'
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
